4e3e52b2f10b8b0e0ae77fff921d6e0e832504c7
baoren <xdman1221@gmail.com>
2022-06-15 04:02:44 +0800

    BackEnd Service Done

--- BackEnd/functions/index.js
+++ BackEnd/functions/index.js
@@ -1,9 +1,34 @@
+// const { response } = require("express");
+const express = require("express");
 const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const app = express();
+app.use(express.json());
+app.get("/", (req, res, next)=> res.json({message: "Firebase service is working!"}));
+// app.get("/todos", (req,res,next)=> res.json({ message: "Get a list of todos"}));
+// Get Hello World
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  // functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+// Get the Hi
+exports.hi = functions.https.onRequest((request, response)=> {
+  response.json({message: "Hi There!", sender: "Firebase Function"});
+});
+
+
+const storeRouter = require("./controllers/store_controller");
+app.use("/store", storeRouter);
+
+exports.api = functions.https.onRequest(app);
+
+// To handle "Function Timeout" exception
+exports.functionsTimeOut = functions.runWith({
+  timeoutSeconds: 300,
+});
+exports.setupstore = functions.https.onRequest(require("./setup_store"));
+
